d20af3dada9da6f0aedadea571d8061807b4fdf5
Muddz <aux-cc@hotmail.com>
2017-12-24 19:22:37 +0100

    new demo app 40% done

--- demo/src/main/java/com/muddzdev/styleabletoast/MainActivity.java
+++ demo/src/main/java/com/muddzdev/styleabletoast/MainActivity.java
@@ -1,94 +1,111 @@
 package com.muddzdev.styleabletoast;
 
+import android.annotation.SuppressLint;
 import android.graphics.Color;
-import android.graphics.Paint;
-import android.graphics.Typeface;
+import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatSeekBar;
 import android.util.TypedValue;
 import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.SeekBar;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import com.muddzdev.styleabletoastlibrary.StyleableToast;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
-import java.lang.reflect.Type;
+public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBarChangeListener {
 
-import static android.util.TypedValue.COMPLEX_UNIT_SP;
 
-public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.corner_value_txv)
+    TextView cornerValueTxv;
+    @BindView(R.id.corner_radius_sb)
+    AppCompatSeekBar cornerRadiusSb;
+    @BindView(R.id.stroke_value_txv)
+    TextView strokeValueTxv;
+    @BindView(R.id.stroke_width_sb)
+    AppCompatSeekBar strokeWidthSb;
+    @BindView(R.id.strokecolor_btn)
+    Button strokecolorBtn;
+    @BindView(R.id.backgroundcolor_btn)
+    Button backgroundcolorBtn;
+    @BindView(R.id.textcolor_btn)
+    Button textcolorBtn;
+    @BindView(R.id.textbold_checkbox)
+    CheckBox textboldCheckbox;
+    @BindView(R.id.solidbackground_checkbox)
+    CheckBox solidbackgroundCheckbox;
+    @BindView(R.id.icon_left)
+    ImageView iconLeft;
+    @BindView(R.id.textview)
+    TextView textview;
+    @BindView(R.id.icon_right)
+    ImageView iconRight;
+    @BindView(R.id.demo_toast)
+    LinearLayout demoToast;
 
-    private StyleableToast styleableToast;
 
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        textview.setText("Hello World");
+        cornerRadiusSb.setOnSeekBarChangeListener(this);
+        strokeWidthSb.setOnSeekBarChangeListener(this);
+
+        strokeValueTxv.setText(strokeWidthSb.getProgress() + "dp");
+        cornerValueTxv.setText(cornerRadiusSb.getProgress() + "dp");
+
     }
 
-    public void Toaster(View v) {
-        switch (v.getId()) {
-            case R.id.button1:
-                StyleableToast.makeText(this, "Uploading image", R.style.allStyles).show();
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+        switch (seekBar.getId()) {
+
+            case R.id.corner_radius_sb:
+                setCornerRadius(i);
                 break;
 
-            case R.id.button2:
-                StyleableToast.makeText(this, "Image saved", Toast.LENGTH_LONG, R.style.style2).show();
-                break;
-
-            case R.id.button3:
-
-                new StyleableToast
-                        .Builder(this)
-                        .text("Turn off flight mode")
-                        .iconResLeft(R.drawable.ic_airplanemode_inactive_black_24dp)
-                        .backgroundColor(Color.parseColor("#865aff"))
-                        .show();
-
-                break;
-
-            case R.id.button4:
-
-                new StyleableToast
-                        .Builder(this)
-                        .text("Updating profile")
-                        .textColor(Color.WHITE)
-                        .backgroundColor(Color.parseColor("#ff5a5f"))
-                        .iconResLeft(R.drawable.ic_autorenew_black_24dp)
-                        .show();
-
-
-                break;
-
-            case R.id.button5:
-                int strokeWidth2 = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 1f, getResources().getDisplayMetrics());
-                new StyleableToast
-                        .Builder(this)
-                        .text("Milk added to list")
-                        .backgroundColor(Color.parseColor("#74ba82"))
-                        .show();
-                break;
-
-            case R.id.button6:
-
-                int width = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 1, getResources().getDisplayMetrics());
-                int color = Color.parseColor("#c9c9c9");
-
-                new StyleableToast
-                        .Builder(this)
-                        .text("Picture saved to gallery")
-                        .textColor(Color.parseColor("#fc2d3e"))
-                        .textBold()
-                        .stroke(width,color)
-                        .typeface(Typeface.createFromAsset(getAssets(), "fonts/dosis.otf"))
-                            .backgroundColor(Color.parseColor("#dddddd"))
-                        .show();
+            case R.id.stroke_width_sb:
+                setStrokeWidth(i, Color.RED);
                 break;
         }
+    }
 
-        if (styleableToast != null) {
-            styleableToast.show();
-            styleableToast = null;
-        }
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+
+    }
+
+
+    @SuppressLint("SetTextI18n")
+    private void setStrokeWidth(int radius, int color) {
+        GradientDrawable toastShape = (GradientDrawable) ContextCompat.getDrawable(this, R.drawable.styleabletoast_shape);
+        strokeValueTxv.setText(radius + "dp");
+        int radiusDp = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, radius, getResources().getDisplayMetrics());
+        toastShape.setStroke(radiusDp, color);
+        demoToast.setBackground(toastShape);
+    }
+
+    @SuppressLint("SetTextI18n")
+    private void setCornerRadius(int radius) {
+        GradientDrawable toastShape = (GradientDrawable) ContextCompat.getDrawable(this, R.drawable.styleabletoast_shape);
+        cornerValueTxv.setText(radius + "dp");
+        int radiusDp = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, radius, getResources().getDisplayMetrics());
+        toastShape.setCornerRadius(radiusDp);
+        demoToast.setBackground(toastShape);
     }
 }
